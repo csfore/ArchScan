6b2c55ba117cad629dbb053dd413327e4de8fc95
Christopher Fore <contactme@chrisfore.us>
2022-08-23 14:25:24 -0400

    Tidied up unused code | Added comments | Expanded upon README.md

--- README.md
+++ README.md
@@ -1,4 +1,15 @@
 # ArchScan
 
 This program is written with the intentions of providing information whether a package you have installed
-shows up in the "Pacman & Package Upgrade Issues" thread on the Arch Forums.
+shows up in the "Pacman & Package Upgrade Issues" thread on the Arch Forums. Only the most recent packages! (May change)
+
+## Using
+
+For right now, this only works when you run it, I will eventually implement either a cron instance or systemd support.
+
+THIS MAY BE CONVERTED TO A DIFFERENT LANGUAGE IN THE NEAR FUTURE, KEEP THAT IN MIND
+
+## Contact
+
+If you wish to contact me, you can reach me via email [contactme@chrisfore.us](emailto:contactme@chrisfore.us)
+or through the Issues tab.
--- main.py
+++ main.py
@@ -2,69 +2,64 @@ import requests
 from bs4 import BeautifulSoup
 import subprocess
 
-URL = "https://bbs.archlinux.org/viewforum.php?id=44"
-solvedCounter = 0
+
+
 lent = 0
 
 
 # Making the function to send notifications via system subprocesses
-def notify(solved, unresolved):
-    message = f"Arch Packages update:\n{solved} solved\n{unresolved} open"
+def notify(solved, unresolved, hits):
+    message = f"Arch Packages update:\n{solved} solved\n{unresolved} open\nYou had {hits} hits"
     subprocess.Popen(['notify-send', message])
     return
 
 
-page = requests.get(URL)
+# Setting the URL to the proper thread
+URL = "https://bbs.archlinux.org/viewforum.php?id=44"
 
+# Getting the contents of the page
+page = requests.get(URL)
 initial = BeautifulSoup(page.content, "lxml")
 
+# Finding the "tbody" (table body) elements
 results = initial.find("tbody")
 
+# Converting the results to a string, then parsing them again
 links = BeautifulSoup(str(results), "html.parser")
-
 raw = []
 
+# Getting all the hyperlinks
 for link in links.find_all('a'):
     raw.append(link.get_text('href'))
 
+# Trimming the first six (these will likely never change, and are the stickied topics)
 rawTrimmed = raw[6:]
 
+# Taking out the date hyperlinks and keeping just the topic names
 packages = []
 while lent < len(rawTrimmed):
     packages.append(rawTrimmed[lent])
     lent += 2
 
+# Figuring out how many of the topics had "[SOLVED]" in the title (might be inaccurate, I saw one with "[solved]"
+solvedAmount = 0
 for entry in packages:
     if entry.startswith("[SOLVED]"):
-        solvedCounter += 1
+        solvedAmount += 1
     else:
         pass
-
-unsolved = len(packages) - solvedCounter
-
-# listPackages = subprocess.Popen(("pacman", "-Qet"), stdout=subprocess.PIPE)
-# awkPackages = subprocess.check_output(("awk", "{print $1}"), stdin=listPackages.stdout)
-# output = listPackages.communicate()[0]
-#
-# print(output)
+unsolvedAmount = len(packages) - solvedAmount
 
 output = subprocess.check_output("pacman -Qet | awk '{print $1}'", shell=True)
-
-#print(output.decode().splitlines())
-#notify(solvedCounter, unsolved)
-
 installedPackages = output.decode().splitlines()
 
-arr = ["abc", "a", "123"]
-
-ar = ["pack", "pac", "p"]
-
-# for x in arr:
-#     for y in ar:
-#         if x in y:
-#             print(f"{x}, {y}")
-
+# Getting the hit count of packages installed vs packages in the topic
+hitCount = 0
 for package in installedPackages:
     for issues in packages:
         if package in issues:
             print(f"Package {package} showed up in issue {issues}")
+            hitCount += 1
+
+# Sending a desktop notification
+notify(solvedAmount, unsolvedAmount, hitCount)
